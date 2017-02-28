ad614354c70af743e58ec6475e7fa5b255406159
Nikita Glukhov <n.gluhov@postgrespro.ru>
2017-02-28 04:03:41 +0300

    Add STRICT/LAX jsonpath mode

--- src/include/catalog/pg_proc.h
+++ src/include/catalog/pg_proc.h
@@ -5545,7 +5545,7 @@ DESCR("jsonpath query");
 DATA(insert OID =  6056 (  _jsonpath_exists		PGNSP PGUID 12 1 0 0 0 f f f f t f i s 3 0 16 "3802 6050 3802" _null_ _null_ _null_ _null_ _null_ jsonb_jsonpath_exists3 _null_ _null_ _null_ ));
 DESCR("jsonpath exists test");
 DATA(insert OID =  6057 (  _jsonpath_query		PGNSP PGUID 12 1 1000 0 0 f f f f t t i s 3 0 3802 "3802 6050 3802" _null_ _null_ _null_ _null_ _null_ jsonb_jsonpath_query3 _null_ _null_ _null_ ));
-DESCR("jsonpath query");
+DESCR("jsonpath object test");
 
 /*
  * Symbolic values for provolatile column: these indicate whether the result
